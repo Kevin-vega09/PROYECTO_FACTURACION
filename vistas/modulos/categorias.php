ee4f15d06ee7a29b8e2032003e7c78b10110c845
Juan <juanestebanmurciagonzalez771@gmail.com>
2024-11-16 01:08:13 -0500

    prueba

--- vistas/modulos/categorias.php
+++ vistas/modulos/categorias.php
@@ -1,6 +1,6 @@
-<!-- Content Wrapper. Contains page content -->
+
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
+    
     <section class="content-header">
         <h1>
             Administrar categorias
@@ -12,10 +12,10 @@
         </ol>
     </section>
 
-    <!-- Main content -->
+ 
     <section class="content">
 
-        <!-- Default box -->
+    
         <div class="box">
             <div class="box-header with-border">
 
@@ -61,7 +61,7 @@
                             <td class="text-uppercase">'.$value["nombre"].'</td>
                             <td>
                                 <div class="btn-group">
-                                    <button class="btn btn-warning btnEditarCategoria" idNombre="'.$value["id"].'" data-toggle="modal" data-target="#modalEditarCategoria"><i class="fa fa-pencil"></i></button>
+                                    <button class="btn btn-warning data-toggle="modal" data-target="#modalEditarCategoria"><i class="fa fa-pencil"></i></button>
                                     <button class="btn btn-danger"><i class="fa fa-times"></i></button>
                                 </div>
                             </td>
@@ -84,6 +84,7 @@
     </section>
 
 </div>
+
 <!-- ====================== 
 MODAL AGREGAR CATEGORIA
 ========================-->
@@ -141,60 +142,13 @@ MODAL AGREGAR CATEGORIA
 
 </div>
 
-<!-- ====================== 
-MODAL EDITAR CATEGORIA
-========================-->
 
-<div id="modalEditarCategoria" class="modal fade" role="dialog">
-    <div class="modal-dialog">
 
-        <div class="modal-content">
-            <form role="form" method="post">
-                <!-- ====================== 
-                CABEZA DEL MODAL
-                ========================-->
-                <div class="modal-header" style="background:#007bff; color:aliceblue">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Editar categoria</h4>
-                </div>
 
-                <!-- ====================== 
-                CUERPO DEL MODAL
-                ========================-->
-                <div class="modal-body">
-                    <div class="box-body">
 
-                        <!-- Entrada para el Nombre -->
-                        <div class="form-group">
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="fa fa-th"></i></span>
-                                <input type="text" class="form-control input-lg" name="editarCategoria" id="editarCategoria" required>
-                            </div>
-                        </div>
 
-                        </div>
 
-                    </div>
-                </div>
-                <!-- ====================== 
-                PIE DEL MODAL
-                ========================-->
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
-                    <button type="submit" class="btn btn-primary">Guardar cambios</button>
-                </div>
 
-                <!--<?php
 
-                $crearCategoria=new ControlCategorias();
-                $crearCategoria->ctrCrearCategoria();
 
-                ?> -->
-
-            </form>
-
-        </div>
-
-    </div>
-
-</div>
+                            
